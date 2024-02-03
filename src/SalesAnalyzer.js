c3e8020ebf9caa14b5b97e19426ff853281d07ed
samuel wanyoike <wanyoikesamuel42@gmail.com>
2024-02-03 14:43:22 +0300

    initial commit

---
+++ src/SalesAnalyzer.js
@@ -0,0 +1,44 @@
+
+//import all the require module
+
+//Write try and catch and handle the exceptions where ever require
+//return the callback with appropriate values in the methods 
+
+//More userdefined methods can be written if required to write the logical stuff
+
+////This method will read the file content the first parameter is filename and 
+//second is a callback
+ //create array name it as  fileContents
+const readFileContents = (fileName, cb) => {
+ 
+   
+ 
+   //push row by row data in the array created
+
+}
+
+// Use Lodash to filter the data this method will take first parameter
+//as fileContents and second parameter as a callback
+const filterData = (fileContents, cb) => {
+  let filteredData ;
+  
+  cb(null, filteredData);
+}
+
+//This method will writeFile data to output.txt file
+//it is taking parameters are filteredData and a callback
+//filteredata will be given by the filterData method
+const writeFilteredDataToFile = (filteredData, cb) => {
+  try {
+    //use writeFile method and write the filteredData in output.txt file
+  
+  } catch (err) {}
+    
+}
+
+
+module.exports = {
+  readFileContents,
+  filterData,
+  writeFilteredDataToFile
+}
